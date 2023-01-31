e8e5fe81ea9129e7b0e50b92d37d3b3d61b458c4
박인혁 <ryuuki@bag-inhyeog-ui-MacBookAir.local>
2023-01-31 22:38:50 +0900

    Memorize project추가

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,142 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by 박인혁 on 2022/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var emojis = ["🍎","🍐","🍊","🍌","🍉","🍓","🫐","🍇","🍑","🧄","🌽","🍆","🥑","🥦","🧅","🫑","🍔","🌭","🧇","🥩","🍗","🍕"]
+    
+    @State var emojiCount = 3
+    
+    var body: some View {
+        VStack{
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 67))]){
+                    ForEach (emojis[0..<emojiCount], id: \.self){emoji in
+                        CardView(content: emoji).aspectRatio(2/3,contentMode:.fit)
+                    }
+                }
+                
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                shuffle
+                Spacer()
+                add
+                
+                
+            }
+            .padding(.horizontal)
+            .font(.largeTitle)
+            
+        }
+        .padding(.all)
+        
+    }
+    var remove: some View{
+        Button{
+            if emojiCount > 1{
+                emojiCount-=1}
+        }
+    label:{
+        Image(systemName: "minus.circle")
+    }
+        
+    }
+    var add: some View{
+        Button{
+            if emojiCount<emojis.count{
+                emojiCount += 1}
+            
+        } label:{
+            Image(systemName: "plus.circle")
+        }
+        
+        
+    }
+    var shuffle: some View{
+        Button{
+            
+        }label: {
+            Text("shuffle")
+        }
+    }
+    
+    
+    
+}
+
+
+struct CardView: View{
+    @State var isFaceUP:Bool = false
+    var content: String
+    
+    var body: some View{
+        
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius:25)
+            
+            if isFaceUP{
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth:3)
+                Text(content)
+                    .font(.largeTitle)
+            }else{
+                shape.fill()
+            }
+            
+        }
+        .onTapGesture{
+            isFaceUP = !isFaceUP
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View  {
+        ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 박인혁 on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene  {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
